a84aae2726ee4e3fda8fed6730a7dd7f8bd2db03
Leandro Nishijima <4566637+leandronishijima@users.noreply.github.com>
2022-08-10 22:16:31 -0300

    wrong_live win logic + live_patch

--- lib/pento_web/live/wrong_live.ex
+++ lib/pento_web/live/wrong_live.ex
@@ -1,9 +1,20 @@
 defmodule PentoWeb.WrongLive do
   use PentoWeb, :live_view
 
+  alias __MODULE__
+
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, score: 0, message: "Guess a game")}
+    {
+      :ok,
+      assign(
+        socket,
+        number_to_guess: :rand.uniform(10),
+        score: 0,
+        win?: false,
+        message: "Guess a game"
+      )
+    }
   end
 
   @impl true
@@ -15,26 +26,44 @@ defmodule PentoWeb.WrongLive do
         It's <%= time() %>
       </h2>
       <h2>
-        <%= for n <- 1..10 do %>
-        <a href="#" phx-click="guess" phx-value-number="<%= n %>"><%= n %></a>
+        <%= unless @win? do %>
+          <%= for n <- 1..10 do %>
+            <a href="#" phx-click="guess" phx-value-number="<%= n %>"><%= n %></a>
+          <% end %>
+        <% else %>
+          <%= live_patch "Reset game", to: Routes.live_path(@socket, WrongLive) %>
         <% end %>
       </h2>
     """
   end
 
-  def handle_event("guess", %{"number" => guess} = data, socket) do
-    IO.inspect(data)
-    message = "Your guess: #{guess}. Wrong. Guess again. "
-    score = socket.assigns.score - 1
+  @impl true
+  def handle_event("guess", %{"number" => guess}, socket) do
+    if socket.assigns.number_to_guess == String.to_integer(guess) do
+      score = socket.assigns.score + 1
 
-    {
-      :noreply,
-      assign(
-        socket,
-        message: message,
-        score: score
-      )
-    }
+      {
+        :noreply,
+        assign(
+          socket,
+          message: "You win!",
+          win?: true,
+          score: score
+        )
+      }
+    else
+      message = "Your guess: #{guess}. Wrong. Guess again. "
+      score = socket.assigns.score - 1
+
+      {
+        :noreply,
+        assign(
+          socket,
+          message: message,
+          score: score
+        )
+      }
+    end
   end
 
   defp time do
